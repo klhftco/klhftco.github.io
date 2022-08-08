9d8a93610f37a07a128fee66ad52ee293c77d131
klhftco <klhftco@gmail.com>
2022-08-07 18:33:39 -0700

    start site

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+  btn.classList.add('animating');
+});
+
+// var path = document.querySelector('path');
+// var length = path.getTotalLength();
+// console.log(length);
