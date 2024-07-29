18c39b5a1952b8eb75c3f1e521b23bc0f95ac5f8
Leo Huang <klhftco@berkeley.edu>
2024-07-28 22:27:25 -0700

    finish clickable cards css implementation

--- website/script.js
+++ website/script.js
@@ -1,7 +1,7 @@
 const body = document.body;
 
-const btnTheme = document.querySelector(".fa-moon");
-const btnHamburger = document.querySelector(".fa-bars");
+// const btnTheme = document.querySelector(".fa-moon");
+// const btnHamburger = document.querySelector(".fa-bars");
 
 // const addThemeClass = (bodyClass, btnClass) => {
 //   body.classList.add(bodyClass);
